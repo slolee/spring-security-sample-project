489fb315e5876d11a246854c5dd77656fbb656f5
slolee <slolee@naver.com>
2022-09-17 14:54:56 +0900

    Feat: Init Commit

---
+++ src/main/java/com/example/springsecuritylivecodingpractice/SpringSecurityLiveCodingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritylivecodingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLiveCodingPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLiveCodingPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecuritylivecodingpractice/SpringSecurityLiveCodingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritylivecodingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLiveCodingPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
