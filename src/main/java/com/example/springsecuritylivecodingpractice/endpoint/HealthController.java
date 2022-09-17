fd3c397b4d0597a7ccab54f6f61533ffd47ac03c
slolee <slolee@naver.com>
2022-09-17 14:56:50 +0900

    Feat: HealthController 추가

---
+++ src/main/java/com/example/springsecuritylivecodingpractice/endpoint/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.springsecuritylivecodingpractice.endpoint;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+
+}
